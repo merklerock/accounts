f982b5e797a80584916b7e74e042eafb68d84aa8
idjango <ismaeldjango@gmail.com>
2014-02-03 06:43:04 +0100

    20140202.1

---
+++ client/js/services.js
@@ -0,0 +1,69 @@
+'use strict';
+
+angular.module('angular-client-side-auth')
+.factory('Auth', function($http, $cookieStore){
+
+    var accessLevels = routingConfig.accessLevels
+        , userRoles = routingConfig.userRoles
+        , currentUser = $cookieStore.get('user') || { username: '', role: userRoles.public };
+
+    $cookieStore.remove('user');
+
+    function changeUser(user) {
+        _.extend(currentUser, user);
+    };
+
+    return {
+        authorize: function(accessLevel, role) {
+            if(role === undefined)
+                role = currentUser.role;
+
+            return accessLevel.bitMask & role.bitMask;
+        },
+        isLoggedIn: function(user) {
+            if(user === undefined)
+                user = currentUser;
+            return user.role.title == userRoles.user.title || user.role.title == userRoles.admin.title;
+        },
+        register: function(user, success, error) {
+            $http.post('/register', user).success(function(res) {
+                changeUser(res);
+                success();
+            }).error(error);
+        },
+        login: function(user, success, error) {
+            $http.post('/login', user).success(function(data, status, headers, config){
+                $window.sessionStorage.token = data.token;
+                var encodedProfile = data.token.split('.')[1];
+                var user = JSON.parse(url_base64_decode(encodedProfile));
+                changeUser(user);
+                success(user);
+                $cookieStore.put('user', user);
+            }).error(function (data, status, headers, config) {
+                // Erase the token if the user fails to log in
+                delete $window.sessionStorage.token;
+            });
+        },
+        logout: function(success, error) {
+            $http.post('/logout').success(function(){
+                changeUser({
+                    username: '',
+                    role: userRoles.public
+                });
+                success();
+            }).error(error);
+        },
+        accessLevels: accessLevels,
+        userRoles: userRoles,
+        user: currentUser
+    };
+});
+
+angular.module('angular-client-side-auth')
+.factory('Users', function($http) {
+    return {
+        getAll: function(success, error) {
+            $http.get('/users').success(success).error(error);
+        }
+    };
+});
